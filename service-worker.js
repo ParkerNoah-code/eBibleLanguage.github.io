2aff8812d2ec3408c4ecfff6900154a6cff222fa
ParkerNoah <72557251+ParkerNoah@users.noreply.github.com>
2025-05-23 10:20:11 -0400

    edit

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = "ebible-cache-v1";
+const URLS_TO_CACHE = [
+  "./index.html",
+  "./Styles.css",
+  "./manifest.json",
+  "./cross_image.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return cached || fetch(event.request);
+    })
+  );
+});
